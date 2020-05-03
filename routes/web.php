14d6ffa17c65c47e38ab6c3588096541eeba212d
rasool_hashbari <63225171+rasoolhashbari@users.noreply.github.com>
2020-05-03 10:58:55 +0430

    Create 404 and 500 error pages

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,11 @@ Route::get('register', function () {
 Route::get('403', function () {
     return view('errors.403');
 });
+Route::get('404', function () {
+    return view('errors.404');
+});
+Route::get('500', function () {
+    return view('errors.500');
+});
+
 
