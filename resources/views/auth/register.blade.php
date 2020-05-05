3f0650c9684142fb595dabd0bd2c0f8b55efcfb2
rasool_hashbari <63225171+rasoolhashbari@users.noreply.github.com>
2020-05-05 13:14:09 +0430

    edit page rigester

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
                     <!-- Sign Up Block -->
                     <div class="block block-themed block-fx-shadow mb-0">
                         <div class="block-header bg-success">
-                            <h3 class="block-title">ایجاد حساب</h3>
+                            <h3 class="block-title">حساب را ایجاد کنید</h3>
                             <div class="block-options">
                                 <a class="btn-block-option font-size-sm" href="javascript:void(0)" data-toggle="modal" data-target="#one-signup-terms">مشاهده قوانین</a>
                                 <a class="btn-block-option" href="op_auth_signin.html" data-toggle="tooltip" data-placement="left" title="Sign In">
@@ -24,35 +24,37 @@
                         </div>
                         <div class="block-content">
                             <div class="p-sm-3 px-lg-4 py-lg-5">
-                                <h1 class="mb-2 px-9">OneUI</h1>
-                                <p class=" text-right">.لطفاً برای ایجاد یک حساب کاربری ، جزئیات زیر را پر کنید</p>
+                                <h1 class="mb-2">OneUI</h1>
+                                <p>.لطفاً برای ایجاد یک حساب کاربری ، جزئیات زیر را پر کنید</p>
 
                                 <!-- Sign Up Form -->
+                                <!-- jQuery Validation (.js-validation-signup class is initialized in js/pages/op_auth_signup.min.js which was auto compiled from _es6/pages/op_auth_signup.js) -->
+                                <!-- For more info and examples you can check out https://github.com/jzaefferer/jquery-validation -->
                                 <form class="js-validation-signup" action="be_pages_auth_all.html" method="POST">
                                     <div class="py-3">
                                         <div class="form-group">
-                                            <input type="text" class="form-control form-control-lg form-control-alt text-right" id="signup-username" name="signup-username" placeholder="نام کاربری">
+                                            <input type="text" class="form-control form-control-lg form-control-alt" id="signup-username" name="signup-username" placeholder="نام کاربری">
                                         </div>
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-lg form-control-alt text-right" id="signup-email" name="signup-email" placeholder="ایمیل">
+                                            <input type="email" class="form-control form-control-lg form-control-alt" id="signup-email" name="signup-email" placeholder="ایمیل">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-lg form-control-alt text-right" id="signup-password" name="signup-password" placeholder="گذرواژه">
+                                            <input type="password" class="form-control form-control-lg form-control-alt" id="signup-password" name="signup-password" placeholder="رمز عبور">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-lg form-control-alt text-right" id="signup-password-confirm" name="signup-password-confirm" placeholder="تایید گذرواژه">
+                                            <input type="password" class="form-control form-control-lg form-control-alt" id="signup-password-confirm" name="signup-password-confirm" placeholder="تایید گذرواژه">
                                         </div>
                                         <div class="form-group">
-                                            <div class="custom-control custom-checkbox ">
+                                            <div class="custom-control custom-checkbox">
                                                 <input type="checkbox" class="custom-control-input" id="signup-terms" name="signup-terms">
-                                                <label class="custom-control-label font-w400 ml-xl-7"for="signup-terms">من با شرایط و ضوابط موافقم </label>
+                                                <label class="custom-control-label font-w400" for="signup-terms">من با شرایط و ضوابط موافقم</label>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="form-group row">
                                         <div class="col-md-6 col-xl-5">
                                             <button type="submit" class="btn btn-block btn-success">
-                                                <i class="fa fa-fw fa-plus mr-1"></i>ثبت نام
+                                                <i class="fa fa-fw fa-plus mr-1"></i> ثبت نام
                                             </button>
                                         </div>
                                     </div>
@@ -73,36 +75,5 @@
 </main>
 <!-- END Main Container -->
 </div>
-<!-- END Page Container -->
-
-<!-- Terms Modal -->
-<div class="modal fade" id="one-signup-terms" tabindex="-1" role="dialog" aria-labelledby="one-signup-terms" aria-hidden="true">
-<div class="modal-dialog modal-lg modal-dialog-popout" role="document">
-    <div class="modal-content">
-        <div class="block block-themed block-transparent mb-0">
-            <div class="block-header bg-primary-dark">
-                <h3 class="block-title">Terms &amp; Conditions</h3>
-                <div class="block-options">
-                    <button type="button" class="btn-block-option" data-dismiss="modal" aria-label="Close">
-                        <i class="fa fa-fw fa-times"></i>
-                    </button>
-                </div>
-            </div>
-            <div class="block-content">
-                <p>Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>
-                <p>Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>
-                <p>Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>
-                <p>Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>
-                <p>Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>
-            </div>
-            <div class="block-content block-content-full text-right border-top">
-                <button type="button" class="btn btn-sm btn-link mr-2" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal">I Agree</button>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-<!-- END Terms Modal -->
 
 @endsection
